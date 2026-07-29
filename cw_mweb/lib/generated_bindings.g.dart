304cbb752eddbe882a184b368e9cc220c56243ad
OurWallet <ci@ourwallet.com>
2026-07-28 21:15:56 -0500

    Build #29 - fix Windows: CXXFLAGS for coroutine + stub MWebFlutter methods

--- cw_mweb/lib/generated_bindings.g.dart
+++ cw_mweb/lib/generated_bindings.g.dart
@@ -5,4 +5,8 @@ import 'dart:ffi';
 
 class MWebFlutter {
   MWebFlutter(DynamicLibrary lib);
+
+  int StartServer(Pointer<Char> chain, Pointer<Char> dataDir, Pointer<Char> nodeUri, Pointer<Pointer<Char>> errMsg) => 0;
+  void StopServer() {}
+  Pointer<Char> Addresses(Pointer<Void> scanSecret, int scanSecretLen, Pointer<Void> spendPub, int spendPubLen, int fromIndex, int toIndex) => nullptr;
 }
