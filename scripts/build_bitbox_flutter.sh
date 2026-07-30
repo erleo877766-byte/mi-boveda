7333fba5cfe5e0885cdd509e3c7a0398072a1f74
OurWallet <ci@ourwallet.com>
2026-07-29 23:32:35 -0500

    fix: use scripts/bitbox_flutter dir and copy api.aar to android/libs

--- scripts/build_bitbox_flutter.sh
+++ scripts/build_bitbox_flutter.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -x -e
 
-if [[ ! -d "bitbox_flutter" ]];
-then
-    git clone https://github.com/konstantinullrich/bitbox_flutter
-fi
-
-cd bitbox_flutter
-git fetch -a
-git reset --hard
-git checkout 5a6e6dd388ef64003f86094af80d5453518b601d
-git reset --hard
+cd scripts/bitbox_flutter
 
 bash ./build_bindings.sh --dont-install
 
-FILE=api.aar
+FILE=go/api/api.aar
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
+    mkdir -p android/libs
+    cp "$FILE" android/libs/
+else
+    echo "ERROR: $FILE not found!"
+    exit 1
 fi
